184f308d10c7dcd20bda52672616cb6d704648a5
Kseniya8765 <tomashevich.kseniya@bk.ru>
2019-10-10 18:52:39 +0200

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.game = 0;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+    this.game = Math.round((this.max + this.min)/ 2);
+    return this.game;
     }
 
     lower() {
-
+this.max = this.game;
     }
 
     greater() {
-
+this.min = this.game;
     }
 }
 
